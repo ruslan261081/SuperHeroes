4fef221f2354d700cfc8bc821a4a5ce8d468c18f
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2019-02-19 21:52:16 +0200

    Add files via upload

---
+++ HW170219/ISuperHero.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW170219
+{
+    interface ISuperHero
+    {
+        void ActivateSuperPowers();
+        
+    }
+    interface IFlash : ISuperHero
+    {
+        void FireLightnings();
+    }
+    interface IClimb : ISuperHero
+    {
+        void Climb();
+    }
+    interface IFly : ISuperHero
+    {
+        void Fly();
+    }
+
+    abstract class Human
+    {
+        public abstract string Name { get; set; }
+
+        private int age;
+
+        public  int AgeGet
+        {
+            get
+            {
+                return this.age;
+            }
+        }
+
+        protected Human(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"The Name is: {Name}, age is: {age}";
+        }
+    }
+    class Flash : Human, IFlash 
+    {
+        public int Voltage{get; private set;}
+        public override string Name { get; set; }
+
+        public Flash(int voltage, string name, int age) : base(name, age)
+        {
+            Voltage = voltage;
+
+        }
+       
+        public void ActivateSuperPowers()
+        {
+            Console.WriteLine("Flash is superhero !!! ");
+        }
+
+        public void FireLightnings()
+        {
+            Console.WriteLine("");
+        }
+
+        public override string ToString()
+        {
+            return $"Voltage is: {Voltage} " + base.ToString();
+        }
+    }
+    class SpiderMan : Human, IClimb
+    {
+        public int Speed { get; private set; }
+        public override string Name { get; set; }
+
+        public SpiderMan(int speed, string name, int age) : base(name,age)
+        {
+            Speed = speed;
+
+        }
+
+
+        public void ActivateSuperPowers()
+        {
+            Console.WriteLine("SpiderMan can walk the wall");
+        }
+
+        public void Climb()
+        {
+            Console.WriteLine("SpiderMan can Climb everyone");
+        }
+
+        public override string ToString()
+        {
+            return $"Speed is: {Speed} " + base.ToString();
+        }
+    }
+    class SuperMan : Human, IFly
+    {
+        public int WebLeft { get; private set; }
+        public override string Name { get; set; }
+
+        public SuperMan(int webleft, string name, int age) : base(name,age)
+        {
+            WebLeft = webleft;
+        }
+
+        public void ActivateSuperPowers()
+        {
+            Console.WriteLine("SuperMan help people in truble");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("I Belive a can fly, I Belive a can touch to sky");
+        }
+
+        public override string ToString()
+        {
+            return $"Webleft Is: {WebLeft} " + base.ToString();
+        }
+    }
+
+
+
+}
---
+++ HW170219/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW170219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flash hero1 = new Flash(300, "Moshe", 31);
+            SpiderMan hero2 = new SpiderMan(275, "Menashe", 25);
+            SuperMan hero3 = new SuperMan(1000, "Jakob", 27);
+
+            ISuperHero[] superHeroes = new ISuperHero[3];
+            superHeroes[0] = hero1;
+            superHeroes[1] = hero2;
+            superHeroes[2] = hero3;
+            Console.WriteLine("Good Look SuperHeroes");
+
+            foreach(object HeroPerson in superHeroes)
+            {
+                IdentifyHero((ISuperHero)HeroPerson);
+                ActivateHero((ISuperHero)HeroPerson);
+                GetMoreHeroData((ISuperHero)HeroPerson);
+
+            }
+            Console.ReadLine();
+        }
+        static void IdentifyHero(ISuperHero hero)
+        {
+            if(hero is Flash)
+            {
+                Console.WriteLine("Flash is coming");
+            }
+            else if(hero is SpiderMan)
+            {
+                Console.WriteLine("SpiderMan is comin to you");
+            }
+            else if(hero is SuperMan)
+            {
+                Console.WriteLine("SuperMan in the sky");
+            }
+            else
+            {
+                Console.WriteLine("Heroes in holiday today");
+            }
+        }
+        static void ActivateHero(ISuperHero hero)
+        {
+            hero.ActivateSuperPowers();
+        }
+        static void GetMoreHeroData(ISuperHero hero)
+        {
+            if(hero is Flash)
+            {
+                Console.WriteLine("Flash is coming");
+                Console.WriteLine((hero as Flash).Voltage);
+            }
+            else if(hero is SpiderMan)
+            {
+                Console.WriteLine("SpiderMan is coming");
+                Console.WriteLine((hero as SpiderMan).Speed);
+            }
+            else if(hero is SuperMan)
+            {
+                Console.WriteLine("SuperMan in the sky");
+                Console.WriteLine((hero as SuperMan).WebLeft);
+            }
+            else
+            {
+                Console.WriteLine("Heroes in holiday today");
+            }
+        }
+        static ISuperHero CreateHero(string HeroName)
+        {
+            Flash option1 = new Flash(375, HeroName, 30);
+            SpiderMan option2 = new SpiderMan(450, HeroName, 21);
+            SuperMan option3 = new SuperMan(1001, HeroName, 35);
+
+            switch(HeroName)
+            {
+                case ("Flash"): return option1;
+                case ("SpiderMan"): return option2;
+                case ("Superman"): return option3;
+                default:return null;
+            }
+        }
+    }
+}
